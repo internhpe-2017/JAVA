03a3f9a5cdfbb624ce3319e677356b800af9728b
NivethaRaniN <nivetha-rani.n@hpe.com>
2017-03-23 21:10:39 +0530

    CANCEL.JAVA-NIVETHA

---
+++ src/com/infrastructure/conrollerframework/controller/Cancel.java
@@ -0,0 +1,35 @@
+package com.infrastructure.conrollerframework.controller;
+
+import javax.servlet.ServletConfig;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.infrastructure.conrollerframework.data.VI;
+import com.infrastructure.exception.BaseException;
+
+public class Cancel extends BaseAction{
+
+	@Override
+	public VI convertRequest(HttpServletRequest request) throws BaseException {
+		System.out.println("convertRequest");
+		return null;
+	}
+
+	@Override
+	public void actionForward(VI response, ServletConfig config,
+			HttpServletRequest servletRequest,
+			HttpServletResponse servletResponse) throws BaseException {
+		try {
+
+			System.out.println(" Action Forward Called");
+			servletRequest.setAttribute("RESPONSE_VI_ATTRIBUTE", response);
+
+			config.getServletContext().getRequestDispatcher("/welcome.jsp")
+					.forward(servletRequest, servletResponse);
+		}catch(Exception e){
+			e.printStackTrace();
+		}
+		
+	}
+
+}
